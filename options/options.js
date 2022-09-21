1db9a3d795446ac546c05efea57ae031e03a1ca7
Aline Tavares <alaisgomes@gmail.com>
2022-09-21 15:55:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "scaffold_change_test_2",
+  url: "https://scaffold_change_test_2.botics.co",
+  api: "https://scaffold_change_test_2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
